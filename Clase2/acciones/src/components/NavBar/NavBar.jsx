c1d3cfe8fc65d6b58c53e1c3f685017076c61358
matias.varela <jmatiasv@gmail.com>
2022-05-25 22:50:33 -0300

    modificando

---
+++ Clase2/acciones/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,38 @@
+
+
+const NavBar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#"></a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02"
+          aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarColor02">
+          <ul className="navbar-nav mx-auto">
+            <li className="nav-item">
+              <a className="nav-link active" href="#">Inicio
+                <span className="visually-hidden">(current)</span>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#elEstudio">FinanSolution</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#nS">Nosotros</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#aP">Por qué elegirnos</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#clientesWeb">Comprar</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default NavBar
